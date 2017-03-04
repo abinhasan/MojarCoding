af6372cdcbd7504d2f6bbaf5f8f53657255a5db2
abinhasan <porag214@gmail.com>
2017-03-04 22:33:11 +0600

    add new api link in app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,8 @@ var serviceBase = 'http://localhost:12050/';
 
 //var serviceBase = 'http://technology.somee.com/';
 
+//var serviceBase = 'http://abinhasan-001-site1.ctempurl.com/';
+
 app.constant('ngAuthSettings', {
 	apiServiceBaseUri: serviceBase
 });
